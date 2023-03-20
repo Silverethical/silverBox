398fd2fa98ef8c69a13059af342be1c6c6fd0161
Majid Kargar <majidkargar01@gmail.com>
2023-03-20 10:50:08 +0330

    refactor(index): imported alert and input component into index.js

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,3 +1,6 @@
 import { log } from "./helpers/log";
-
+import {inputComponent} from "./components/input"
+import { alertComponent } from "./components/alert";
+console.log(inputComponent());
+console.log(alertComponent());
 log("Test", "2");
