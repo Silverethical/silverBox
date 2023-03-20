7845e0c37337949608d34bacb2d3a5324c61d8d2
Majid Kargar <majidkargar01@gmail.com>
2023-03-20 16:25:59 +0330

    refactor(components): deleted textarea and refactored input

--- public/src/js/components/input.js
+++ public/src/js/components/input.js
@@ -3,10 +3,10 @@
  * @param {*} placeHolder - placer holder
  * @returns 
  */
-export function inputComponent(placeHolder="default placeholder",type="text") {
-	const input = document.createElement("input")
-	input.classList.add("silverBox-inputs")
-	input.setAttribute("type",type)
-	input.setAttribute("placeholder",placeHolder)
+export function inputComponent(placeHolder = "default placeholder", type = "text",elementType="input",elementClassList="silverBox-inputs") {
+	const input = document.createElement(elementType)
+	input.classList.add(elementClassList)
+	input.setAttribute("type", type)
+	input.setAttribute("placeholder", placeHolder)
 	return input
 }
--- public/src/js/components/textarea.js
+++
@@ -1,6 +0,0 @@
-export function textareaComponent(placeHolder="default placeHolder"){
-    const textArea = document.createElement("textarea")
-    textArea.classList.add("silverBox-textArea")
-    textArea.setAttribute("placeholder",placeHolder)
-    return textArea
-}
