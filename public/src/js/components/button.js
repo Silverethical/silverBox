6dffba48042c6a004f1465b350213bf4c618e91c
Majid Kargar <majidkargar01@gmail.com>
2023-03-20 20:26:13 +0330

    refactor(components/button): refactor button component parameters

--- public/src/js/components/button.js
+++ public/src/js/components/button.js
@@ -1,16 +1,20 @@
 /**
- * confirm button component
- * @returns button element
-*/
-function buttonComponent({ text = "Confirm", buttonBgColor = '#fff', buttonColor = "#000", elementUniqueClassList }) {
+ * 
+ * @param {string} text - button text 
+ * @param {string} buttonBgColor - button background color 
+ * @param {string} buttonColor - button color
+ * @param {string} elementUniqueClassList - button classList
+ * @param {string} borderRadius - button border radius
+ * @returns 
+ */
+function buttonComponent({ text = "button", buttonBgColor = '#fff', buttonColor = "#000", elementUniqueClassList, borderRadius = "8px" }) {
+    // 
     let button = document.createElement("button")
     button.style.background = buttonBgColor
-    button.setAttribute("style", `background-color:${buttonBgColor}; color:${buttonColor};`)
+    button.setAttribute("style", `background-color:${buttonBgColor}; color:${buttonColor}; border-radius:${borderRadius};`)
     button.classList.add(elementUniqueClassList, "silverBox-button")
     let buttonText = document.createTextNode(text)
     button.appendChild(buttonText)
     return button
 }
-const body = document.body
-body.appendChild(buttonComponent(buttonComponent({})))
 export default buttonComponent;
