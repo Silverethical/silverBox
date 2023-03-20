fbd1ae86535e195e998ff3f3a4ed68c11ca57a09
Mohammad Kargar <57566624+dofxo@users.noreply.github.com>
2023-03-20 16:51:33 +0330

    refactor(components/silverbox): add a sample config /declared conditions

--- public/src/js/components/silverBox.js
+++ public/src/js/components/silverBox.js
@@ -1,11 +1,52 @@
 // import components
 import { buttonComponent } from "./button";
 import { inputComponent } from "./input";
-import { textareaComponent } from "./textarea";
+
+
+silverBox({
+    timer: 1500,
+    position: 'top-right', //
+    icon: "warning",
+    title: "Title", //
+    html: "<h1>text</h1>", //
+    showCancelButton: false, //
+    showDenyButton: true,
+    showConfirmButton: false, //
+    confirmButtonColor: "#3085d6",//
+    confirmButtonText: "Yes",//
+    confirmButtonIconRight: "/path/to/icon",//
+    cancelButtonColor: "#d33",//
+    cancelButtonText: "Cancel",//
+    denyButtonText: "Deny",//
+    disableConfirmButton: true,//
+    inputs: [
+        {
+            label: "label",
+            type: "text",
+            placeHolder: "test",
+            readOnly: true,
+            afterHtml: "",
+        },
+        {
+            label: "enter email",
+            type: "textarea",
+            placeHolder: "test2",
+            readOnly: false,
+        },
+    ],
+})
+
 
 // comments to be added.
-export function silverBox() {
+export function silverBox(config) {
 
-
-    return 'test'
+    // checks if the config needs an input modal or alertModal
+    if (config.inputs) {
+        // input modal
+    }
+    else {
+        // alert modal
+    }
 }
+
+
--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,3 +1,3 @@
 import { silverBox } from "./components/silverBox"
 
-silverBox()
+
