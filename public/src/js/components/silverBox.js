6ea53cf304539530ac2b996bd4aa8a128953fbe9
Mohammad Kargar <57566624+dofxo@users.noreply.github.com>
2023-03-20 16:36:03 +0330

    refactor(components/silverbox): changed the import for button comp

--- public/src/js/components/silverBox.js
+++ public/src/js/components/silverBox.js
@@ -1,26 +1,11 @@
 // import components
-import { confirmButton } from "./confirmButton";
-import { cancelButton } from "./cancelButton";
+import { buttonComponent } from "./button";
 import { inputComponent } from "./input";
 import { textareaComponent } from "./textarea";
 
 // comments to be added.
 export function silverBox() {
 
-    // ---- silverBox modal START ----
 
-    // body
-    const body = document.body
-    // modal box
-    const modalBox = document.createElement("div")
-    modalBox.classList.add("silver-box")
-    // adding the box to body
-    body.appendChild(modalBox)
-    // ---- silverBox modal END ----
-
-    modalBox.appendChild(confirmButton())
-    modalBox.appendChild(cancelButton())
-    modalBox.appendChild(textareaComponent("my placeHolder"))
-    modalBox.appendChild(inputComponent("myPlaceHolder","password"))
-    return modalBox
+    return 'test'
 }
