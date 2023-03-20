29bd2bba0ce7d670a6bbc4a911792e1aa988b5d9
Majid Kargar <majidkargar01@gmail.com>
2023-03-20 14:26:18 +0330

    feat(components/confirm button): add confirm button style

--- public/src/js/components/confirmButton.js
+++ public/src/js/components/confirmButton.js
@@ -1,10 +1,13 @@
 /**
  * confirm button component
+ * @param {string} text - button text
  * @returns button element
  */
-export function confirmButton() {
+export function confirmButton(text="confirm") {
     let button = document.createElement("button")
     button.classList.add("silverBox-confirm-button")
+    let buttonText = document.createTextNode(text)
+    button.appendChild(buttonText)
     return button
 
 }
