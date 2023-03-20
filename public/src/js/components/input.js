23630a35ed9ae4f29ab93529fe7f8bcdf01ca01e
Mohammad Kargar <57566624+dofxo@users.noreply.github.com>
2023-03-20 20:23:52 +0330

    refactor(components/input): creates inputBox as a parent with 2 children

--- public/src/js/components/input.js
+++ public/src/js/components/input.js
@@ -1,8 +1,31 @@
-function inputComponent({ placeHolder = "default placeholder", type = "text", elementType = "input", elementClassList = "silverBox-input" }) {
-	const input = document.createElement(elementType)
-	input.classList.add(elementClassList)
-	input.setAttribute("type", type)
-	input.setAttribute("placeholder", placeHolder)
-	return input
+function inputComponent({ elementType = "input", inputType = "text", placeHolder = "default placeholder", readOnly = false, label = 'this is a label', }) {
+
+	// parent and children element creation
+	let inputWrrapper = document.createElement('div')
+	inputWrrapper.classList.add('silverBox-input')
+
+	// label
+	let labelEl = document.createElement("label")
+	labelEl.textContent = label
+
+	// input
+	let inputEl = document.createElement(elementType)
+
+	// general input/textArea configs
+	inputEl.setAttribute('placeholder', placeHolder)
+
+	// input or textArea selection conditions (for adding input types)
+	if (elementType == "input") {
+		inputEl.setAttribute('type', inputType)
+	}
+	// readOnly condition for inputs
+	if (readOnly == true) {
+		inputEl.setAttribute('readonly')
+	}
+
+	inputWrrapper.append(labelEl)
+	inputWrrapper.appendChild(inputEl)
+	return inputWrrapper
 }
 export default inputComponent
+
