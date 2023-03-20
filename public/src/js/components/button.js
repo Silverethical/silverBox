348927c7c93ea0ebd438a946560748d055600d11
Mohammad Kargar <57566624+dofxo@users.noreply.github.com>
2023-03-20 16:34:19 +0330

    refactor(components) : deleted cancelButton and merged it into one file

--- public/src/js/components/button.js
+++ public/src/js/components/button.js
@@ -3,9 +3,10 @@
  * @param {string} text - button text
  * @returns button element
  */
-export function confirmButton(text="Confirm") {
+export function buttonComponent(text = "Confirm", buttonColor = '#000', elementUniqueClassList) {
     let button = document.createElement("button")
-    button.classList.add("silverBox-confirm-button","silverBox-buttons")
+    button.style.background = buttonColor
+    button.classList.add(elementUniqueClassList, "silverBox-buttons")
     let buttonText = document.createTextNode(text)
     button.appendChild(buttonText)
     return button
