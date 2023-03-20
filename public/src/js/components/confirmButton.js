912361652e40237028a5bb220f795196ab079b8c
Majid Kargar <majidkargar01@gmail.com>
2023-03-20 15:19:12 +0330

    refactor(components/confirmButton): deleted useless styles
    also added silverBox-buttons class name

--- public/src/js/components/confirmButton.js
+++ public/src/js/components/confirmButton.js
@@ -5,7 +5,7 @@
  */
 export function confirmButton(text="Confirm") {
     let button = document.createElement("button")
-    button.classList.add("silverBox-confirm-button")
+    button.classList.add("silverBox-confirm-button","silverBox-buttons")
     let buttonText = document.createTextNode(text)
     button.appendChild(buttonText)
     return button
