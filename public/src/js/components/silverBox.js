97c6a7fc29a15021dc59f2f71fd2027e73808170
Majid Kargar <majidkargar01@gmail.com>
2023-03-20 14:24:21 +0330

    feat(components): added silverBox to body
    added some styles to center silverBox in body

--- public/src/js/components/silverBox.js
+++ public/src/js/components/silverBox.js
@@ -3,5 +3,19 @@ import { confirmButton } from "./confirmButton";
 
 // comments to be added.
 export function silverBox() {
+
+    // ---- silverBox modal START ----
+
+    // body
+    const body = document.body
+    // modal box
+    const modalBox = document.createElement("div")
+    modalBox.classList.add("modal-box")
+    // adding the box to body
+    body.appendChild(modalBox)
+    // ---- silverBox modal END ----
+
+    modalBox.appendChild(confirmButton())
     return "Modal";
 }
+console.log(confirmButton());
