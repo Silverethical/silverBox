d29188e7d4cd35390bb2a2b5837e373c943a8f89
Majid Kargar <majidkargar01@gmail.com>
2023-03-20 11:48:39 +0330

    refactor(index): removed alert import

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,6 +1,4 @@
 import { log } from "./helpers/log";
 import {inputComponent} from "./components/input"
-import { alertComponent } from "./components/alert";
 console.log(inputComponent());
-console.log(alertComponent());
 log("Test", "2");
