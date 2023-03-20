2e194fd3b1a83844685511201bd9ff25d81df299
Saber Heydari <silverethical@gmail.com>
2023-03-20 09:37:17 +0330

    refactor(general): change folder structure

---
+++ public/dist/js/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * Logs the given arguments to the console.
+ *
+ * @param {...*} args - The arguments to log.
+ * @returns {void}
+ */
+function log(...args) {
+	console.log(...args);
+}
+
+log("Test", "2");
