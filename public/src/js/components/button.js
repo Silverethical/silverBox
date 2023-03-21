b8b94ce3f23bbbe69973896f5ccd34cb2e79c608
Majid Kargar <majidkargar01@gmail.com>
2023-03-21 11:34:02 +0330

    refactor(component/button): remove/refactor parameters
    buttonColor - borderRadius / buttonBgColor

--- public/src/js/components/button.js
+++ public/src/js/components/button.js
@@ -2,16 +2,14 @@
  * 
  * @param {string} text - button text 
  * @param {string} buttonBgColor - button background color 
- * @param {string} buttonColor - button color
  * @param {string} elementUniqueClassList - button classList
- * @param {string} borderRadius - button border radius
  * @returns 
  */
-function buttonComponent({ text = "button", buttonBgColor = '#fff', buttonColor = "#000", elementUniqueClassList, borderRadius = "8px" }) {
+function buttonComponent({ text = "button", buttonBgColor, elementUniqueClassList }) {
     // 
     let button = document.createElement("button")
     button.style.background = buttonBgColor
-    button.setAttribute("style", `background-color:${buttonBgColor}; color:${buttonColor}; border-radius:${borderRadius};`)
+    if (buttonBgColor) button.setAttribute("style", `background-color:${buttonBgColor};`)
     button.classList.add(elementUniqueClassList, "silverBox-button")
     let buttonText = document.createTextNode(text)
     button.appendChild(buttonText)
