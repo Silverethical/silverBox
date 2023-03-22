facbbd386b91280026c833e81c17cebf744417e5
Mohammad Kargar <57566624+dofxo@users.noreply.github.com>
2023-03-22 20:38:14 +0330

    refactor(components/header): add a condition for returning the Header EL

--- public/src/js/components/header.js
+++ public/src/js/components/header.js
@@ -1,23 +1,32 @@
-function headerComponent({ titleText = "default title", descriptionText = "description text",imageSource}) {
+function headerComponent({ titleText, descriptionText, imageSource }) {
     // header wrapper
     let headerWrapper = document.createElement("header")
+
     // title 
     let title = document.createElement("h2")
     title.classList.add("silverBox-header-title")
     title.textContent = titleText
+
     // description
     let description = document.createElement("div")
     description.classList.add("silverBox-header-description")
     description.innerHTML = descriptionText
+
+    // icon
     let icon = document.createElement("img")
     icon.classList.add("silverBox-header-icon")
-    icon.setAttribute("src",imageSource)
-    headerWrapper.appendChild(icon)
-    // add title to headerWrapper
-    headerWrapper.appendChild(title)
-    // add description to headerWrapper
-    headerWrapper.appendChild(description)
+    icon.setAttribute("src", imageSource)
+
     // add icon to headerWrapper
-    return headerWrapper
+    if (imageSource) headerWrapper.appendChild(icon)
+    // add title to headerWrapper
+    if (titleText) headerWrapper.appendChild(title)
+    // add description to headerWrapper
+    if (descriptionText) headerWrapper.appendChild(description)
+
+    // checks if header Element is empty or not
+    return headerWrapper.childElementCount !== 0 ? headerWrapper : ''
+
+
 }
 export default headerComponent;
