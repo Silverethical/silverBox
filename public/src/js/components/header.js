faad4e22a009a63ec3662b749fb666852c15dde1
dofxo <kargat504@gmail.com>
2023-03-23 16:46:58 +0330

    refactor(components/header): change the method of appending icon

--- public/src/js/components/header.js
+++ public/src/js/components/header.js
@@ -12,18 +12,12 @@ function headerComponent({ titleText, descriptionText, imageSource }) {
     description.classList.add("silverBox-header-description")
     description.innerHTML = descriptionText
 
-    // icon
-    let icon = document.createElement("img")
-    icon.classList.add("silverBox-header-icon")
-    icon.setAttribute("src", imageSource)
-
     // add icon to headerWrapper
-    if (imageSource) headerWrapper.appendChild(icon)
+    if (imageSource) headerWrapper.appendChild(imageSource)
     // add title to headerWrapper
     if (titleText) headerWrapper.appendChild(title)
     // add description to headerWrapper
     if (descriptionText) headerWrapper.appendChild(description)
-
     // checks if header Element is empty or not
     return headerWrapper.childElementCount !== 0 ? headerWrapper : ''
 
