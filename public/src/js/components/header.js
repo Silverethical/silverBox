feb5d5221cefe3c02a9e46f3e1ae9127ba5df0ea
dofxo <kargat504@gmail.com>
2023-03-24 23:45:01 +0330

    feat(components/header): add svg code to CloseButton

--- public/src/js/components/header.js
+++ public/src/js/components/header.js
@@ -3,6 +3,7 @@ import closeButtonOnClick from "./closeButtonOnClick"
 function headerComponent({ titleText, htmlText, simpleText, imageSource, closeButton }) {
     // header wrapper
     let headerWrapper = document.createElement("header")
+    headerWrapper.classList.add('header-wrapper')
 
     // title 
     let title = document.createElement("h2")
@@ -19,8 +20,9 @@ function headerComponent({ titleText, htmlText, simpleText, imageSource, closeBu
     textStructure.textContent = simpleText
     textStructure.classList.add("silverBox-header-description")
 
-    let closeButtonEl = document.createElement("div")
-    closeButtonEl.textContent = "X"
+    // svg of closeButton button
+    let closeButtonEl = document.createElement("span")
+    closeButtonEl.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 512 512"><line x1="368" y1="368" x2="144" y2="144" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/><line x1="368" y1="144" x2="144" y2="368" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>'
     closeButtonEl.onclick = closeButtonOnClick
     closeButtonEl.classList.add("silverBox-close-button")
     if (closeButton) headerWrapper.appendChild(closeButtonEl)
