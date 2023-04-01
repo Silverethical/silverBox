4a228e28a10ed84ef4366492859293f10d6e6295
Saber Heydari <silverethical@gmail.com>
2023-04-01 16:06:02 +0330

    docs(helper functions): add comments to customStringify function

--- public/src/js/helpers/customStringify.js
+++ public/src/js/helpers/customStringify.js
@@ -1,7 +1,20 @@
+/**
+ * Custom stringify function that removes quotes from object keys.
+ *
+ * @param {object} obj - The object to stringify.
+ * @returns {string} A stringified version of the input object with quotes removed from keys.
+ */
 const customStringify = (obj) => {
+	// Convert the object to a JSON string with indentation
 	const jsonString = JSON.stringify(obj, null, 7);
+
+	// Create a regular expression pattern to match quoted keys
 	const pattern = /"([^"]+)"\s*:/g;
+
+	// Replace all quoted keys with unquoted keys
 	const newString = jsonString.replace(pattern, "$1:");
+
+	// Return the updated string
 	return newString;
 };
 
