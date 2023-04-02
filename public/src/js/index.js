690952001a739fbdbcce0302dffdfbf77ec52614
dofxo <kargat504@gmail.com>
2023-04-02 15:01:31 +0330

    refactor(change examplejs): refactor the example js due to new config

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -4,99 +4,99 @@ import teamMembers from "./data/teamMembers";
 import renderTeamMembers from "./helpers/renderTeamMembers";
 import customStringify from "./helpers/customStringify";
 import renderExample from "./components/renderExample";
-silverBox({
-	// timer: 1000,
-	// position: 'top-right', //
-	theme: "light",
-	// alertIcon: "warning",
-	userIcon: "../src/images/anya.png",
-	title: "AYAYA", //
-	centerContent: true,
-	html: "<p>weebs</p>", //
-	text: "simple text",
-	footer: "<p>Abjim footer AYAYA</p>",
-	showCloseButton: true,
+// silverBox({
+// 	// timer: 1000,
+// 	// position: 'top-right', //
+// 	theme: "light",
+// 	// alertIcon: "warning",
+// 	userIcon: "../src/images/anya.png",
+// 	title: "AYAYA", //
+// 	centerContent: true,
+// 	html: "<p>weebs</p>", //
+// 	text: "simple text",
+// 	footer: "<p>Abjim footer AYAYA</p>",
+// 	showCloseButton: true,
 
-	// confirm button
-	confirmButton: {
-		showButton: false,
-		bgColor: "#3085d6",//
-		borderColor: "#3085d6",
-		textColor: "#fff",
-		text: "Confirm", //
-		iconRight: "../src/images/anya.png",//
-		iconLeft: "../src/images/anya.png",//
-		closeOnClick: false,
-	},
+// 	// confirm button
+// 	confirmButton: {
+// 		showButton: false,
+// 		bgColor: "#3085d6",//
+// 		borderColor: "#3085d6",
+// 		textColor: "#fff",
+// 		text: "Confirm", //
+// 		iconRight: "../src/images/anya.png",//
+// 		iconLeft: "../src/images/anya.png",//
+// 		closeOnClick: false,
+// 	},
 
-	// cancel button
-	cancelButton: {
-		showButton: false,
-		bgColor: "#fff",//
-		// borderColor: "#000",
-		textColor: "#000",
-		text: "Cancel", //
-		iconRight: "/path/to/icon",//
-		iconLeft: "/path/to/icon",//
-		closeOnClick: false,
-	},
+// 	// cancel button
+// 	cancelButton: {
+// 		showButton: false,
+// 		bgColor: "#fff",//
+// 		// borderColor: "#000",
+// 		textColor: "#000",
+// 		text: "Cancel", //
+// 		iconRight: "/path/to/icon",//
+// 		iconLeft: "/path/to/icon",//
+// 		closeOnClick: false,
+// 	},
 
-	// deny button
-	denyButton: {
-		showButton: false,
-		bgColor: "#d23",
-		borderColor: "#d23",
-		textColor: "#fff",
-		text: "Deny", //
-		iconRight: "/path/to/icon", //
-		iconLeft: "/path/to/icon", //
-		closeOnClick: false,
-	},
+// 	// deny button
+// 	denyButton: {
+// 		showButton: false,
+// 		bgColor: "#d23",
+// 		borderColor: "#d23",
+// 		textColor: "#fff",
+// 		text: "Deny", //
+// 		iconRight: "/path/to/icon", //
+// 		iconLeft: "/path/to/icon", //
+// 		closeOnClick: false,
+// 	},
 
-	// input: [
-	// 	{
-	// 		label: "Name",
-	// 		type: "text",
-	// 		// placeHolder: "Enter your name",
-	// 		// hint: 'input hint',
-	// 		readOnly: false,
-	// 		inputWidth: '50px',
-	// 		inputHeight: '50px',
-	// 		inputMaxLength: 10,
-	// 		textAlign: 'center',
-	// 		fontSize: '20px',
-	// 		multiplyBy: 1
-	// 	},
-	// 	// {
-	// 	// 	label: "Email",
-	// 	// 	type: "text",
-	// 	// 	// placeHolder: "Enter your email",
-	// 	// 	// hint: 'input hint',
-	// 	// 	readOnly: false,
-	// 	// 	inputWidth: '50px',
-	// 	// 	inputHeight: '50px',
-	// 	// 	inputMaxLength: 1,
-	// 	// 	// textAlign: 'center',
-	// 	// 	fontSize: '20px',
-	// 	// 	multiplyBy: 1
+// 	// input: [
+// 	// 	{
+// 	// 		label: "Name",
+// 	// 		type: "text",
+// 	// 		// placeHolder: "Enter your name",
+// 	// 		// hint: 'input hint',
+// 	// 		readOnly: false,
+// 	// 		inputWidth: '50px',
+// 	// 		inputHeight: '50px',
+// 	// 		inputMaxLength: 10,
+// 	// 		textAlign: 'center',
+// 	// 		fontSize: '20px',
+// 	// 		multiplyBy: 1
+// 	// 	},
+// 	// 	// {
+// 	// 	// 	label: "Email",
+// 	// 	// 	type: "text",
+// 	// 	// 	// placeHolder: "Enter your email",
+// 	// 	// 	// hint: 'input hint',
+// 	// 	// 	readOnly: false,
+// 	// 	// 	inputWidth: '50px',
+// 	// 	// 	inputHeight: '50px',
+// 	// 	// 	inputMaxLength: 1,
+// 	// 	// 	// textAlign: 'center',
+// 	// 	// 	fontSize: '20px',
+// 	// 	// 	multiplyBy: 1
 
-	// 	// },
-	// 	// {
-	// 	// 	label: "Password",
-	// 	// 	type: "password",
-	// 	// 	// placeHolder: "Create a password",
-	// 	// 	// hint: 'Must be at least 8 characters',
-	// 	// 	readOnly: false,
-	// 	// 	inputWidth: '50px',
-	// 	// 	inputHeight: '50px',
-	// 	// 	// inputMaxLength: 1,
-	// 	// 	// textAlign: 'center',
-	// 	// 	fontSize: '20px',
-	// 	// 	multiplyBy: 1
+// 	// 	// },
+// 	// 	// {
+// 	// 	// 	label: "Password",
+// 	// 	// 	type: "password",
+// 	// 	// 	// placeHolder: "Create a password",
+// 	// 	// 	// hint: 'Must be at least 8 characters',
+// 	// 	// 	readOnly: false,
+// 	// 	// 	inputWidth: '50px',
+// 	// 	// 	inputHeight: '50px',
+// 	// 	// 	// inputMaxLength: 1,
+// 	// 	// 	// textAlign: 'center',
+// 	// 	// 	fontSize: '20px',
+// 	// 	// 	multiplyBy: 1
 
-	// 	// }
-	// ]
-});
+// 	// 	// }
+// 	// ]
+// });
 
 /** selectors */
 const sidebar = document.querySelector("aside.sidebar"),
