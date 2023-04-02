ad8a925994c774b5d4f2cd95772f62b43171006c
Saber Heydari <silverethical@gmail.com>
2023-04-02 19:38:17 +0330

    refactor(components/icons): functions related to icons all in one file

--- public/src/js/components/icons.js
+++ public/src/js/components/icons.js
@@ -1,14 +1,3 @@
-import { createIcon, createUserIcon } from "../helpers/createIcons";
-
-// Create an object to store the available icons.
-const icons = {
-	warning: createIcon("silverBox-warning", "!"),
-	success: createIcon("silverBox-tick-mark", "", "inside"),
-	info: createIcon("silverBox-info", "i"),
-	error: createIcon("silverBox-error", "", "x"),
-	question: createIcon("silverBox-question", "?"),
-};
-
 /**
  * Returns an icon element based on the alert icon type and custom icon URL.
  * @param {string} alertIcon - The alert icon type.
@@ -22,5 +11,58 @@ const iconsComponent = (alertIcon, customIcon) => {
 	// Return the requested icon based on alertIcon.
 	return icons[alertIcon];
 };
+
+// Create an object to store the available icons.
+const icons = {
+	warning: createIcon("silverBox-warning", "!"),
+	success: createIcon("silverBox-tick-mark", "", "inside"),
+	info: createIcon("silverBox-info", "i"),
+	error: createIcon("silverBox-error", "", "x"),
+	question: createIcon("silverBox-question", "?"),
+};
+
+/**
+ * Creates an icon element with the specified class name and child element (if any).
+ * @param {string} className - The class name for the icon element.
+ * @param {string} text - The text to display in the icon element (if any).
+ * @param {string} childClass - The class name for a child element (if any).
+ * @returns {Element} - The icon element.
+ */
+function createIcon(className, text, childClass) {
+	// Create a new div element with the specified class name and class.
+	const icon = document.createElement("div");
+	icon.classList = className;
+	icon.classList.add("silverBox-icon");
+
+	// If childClass is defined, create a child div element with the specified class name and append it to the icon element.
+	if (childClass) {
+		const child = document.createElement("div");
+		child.classList = childClass;
+		icon.appendChild(child);
+	}
+	// If text is defined, create a new span element with the text and append it to the icon element.
+	else if (text) {
+		const span = document.createElement("span");
+		span.textContent = text;
+		icon.appendChild(span);
+	}
+
+	return icon;
+}
+
+/**
+ * Creates a user icon element with the specified URL.
+ * @param {string} userIcon - The URL for the user icon.
+ * @returns {Element} - The user icon element.
+ */
+function createUserIcon(userIcon) {
+	// Create a new img element with the specified class and ID, and set its src attribute to the provided URL.
+	const img = document.createElement("img");
+	img.setAttribute("src", userIcon);
+	img.classList.add("silverBox-icon");
+	img.id = "silverBox-custom-icon";
+
+	return img;
+}
 
 export default iconsComponent;
