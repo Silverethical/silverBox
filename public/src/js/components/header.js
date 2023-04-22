6d359fb2ab642473de592be60db6c7218a8ba5de
Majid Kargar <majidkargar01@gmail.com>
2023-04-22 14:55:39 +0430

    refactor(header): change header tag name to div

--- public/src/js/components/header.js
+++ public/src/js/components/header.js
@@ -1,5 +1,5 @@
 /** imports */
-import silverBoxcloseButtonOnClick from "./closeButtonOnClick"
+import silverBoxCloseButtonOnClick from "./closeButtonOnClick"
 
 /**
  * Returns headerWrapper based on given arguments from config
@@ -12,7 +12,7 @@ import silverBoxcloseButtonOnClick from "./closeButtonOnClick"
  */
 function silverBoxHeaderComponent({ titleText, htmlText, simpleText, imageSource, closeButton }) {
     // header wrapper
-    let headerWrapper = document.createElement("header")
+    let headerWrapper = document.createElement("div")
     headerWrapper.classList.add('header-wrapper')
     // icon and closeButton wrapper
     let iconWrapper = document.createElement('div')
@@ -36,7 +36,7 @@ function silverBoxHeaderComponent({ titleText, htmlText, simpleText, imageSource
     // svg of closeButton button
     let closeButtonEl = document.createElement("span")
     closeButtonEl.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 512 512"><line x1="368" y1="368" x2="144" y2="144" style="fill:none;stroke:#667085;stroke-linecap:round;stroke-linejoin:round;stroke-width:33px"/><line x1="368" y1="144" x2="144" y2="368" style="fill:none;stroke:#667085;stroke-linecap:round;stroke-linejoin:round;stroke-width:33px"/></svg>'
-    closeButtonEl.onclick = silverBoxcloseButtonOnClick
+    closeButtonEl.onclick = silverBoxCloseButtonOnClick
     closeButtonEl.classList.add("silverBox-close-button")
 
     // add icon to iconWrapper
