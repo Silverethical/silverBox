b9a04c939e5238abff314e05ced4d5a8cff9daae
dofxo <kargat504@gmail.com>
2023-04-22 21:40:26 +0330

    refactor(documentation): sort documentation alphabetically

--- public/src/js/data/documentation.js
+++ public/src/js/data/documentation.js
@@ -1,122 +1,124 @@
+import sortDocumentation from "../helpers/sortDocumentationArray";
+
 const documentation = [
     {
         configName: "alertIcon",
         defaultValue: "",
-        type:"string",
+        type: "string",
         explanation: `Predefined icons including: "success", "error", "warning", "info", "question".`,
     },
     {
         configName: "customIcon",
         defaultValue: "",
-        type:"string",
+        type: "string",
         explanation: `Path to a customIcon. For example customIcon: "path/to/icon"; .`,
     },
     {
         configName: "timer",
         defaultValue: undefined,
-        type:"string",
+        type: "string",
         explanation: "SilverBox closes after given time in milliseconds."
     },
     {
         configName: "position",
         defaultValue: "",
-        type:"string",
+        type: "string",
         explanation: `Position SilverBox in: "top-right", "top-center", "top-left", "bottom-right", "bottom-center", "bottom-left".`
     },
     {
         configName: "theme",
         defaultValue: "light",
-        type:"string",
+        type: "string",
         explanation: `Changes SilverBox theme. Predefined themes: "light", "dark".`
     },
     {
         configName: "title",
         defaultValue: "",
-        type:"string",
+        type: "string",
         explanation: "SilverBox title."
     },
     {
         configName: "centerContent",
         defaultValue: false,
-        type:"boolean",
+        type: "boolean",
         explanation: `Aligns the content center. "true" or "false".`
     },
     {
         configName: "html",
         defaultValue: "",
-        type:"string",
+        type: "string",
         explanation: `HTML as SilverBox body. If "text" parameter is given at the same time as "html" parameter, "text" will be ignored. example: "&lt;a href='/recovery'&gt;recover password&lt;/a&gt;".`
     },
     {
         configName: "text",
         defaultValue: "",
-        type:"string",
+        type: "string",
         explanation: `Text as SilverBox body. If "text" parameter is given at the same time as "html" parameter, "text" will be ignored.`
     },
     {
         configName: "footer",
         defaultValue: "",
-        type:"string",
+        type: "string",
         explanation: "HTML as SilverBox footer."
     },
     {
-        configName:"showCloseButton",
-        defaultValue:false,
-        type:"boolean",
-        explanation:"Shows a small 'x' on the top of SilverBox."
+        configName: "showCloseButton",
+        defaultValue: false,
+        type: "boolean",
+        explanation: "Shows a small 'x' on the top of SilverBox."
     },
     {
         configName: "confirmButton",
         defaultValue: "emptyDefaultValue",
-        type:"object",
+        type: "object",
         explanation: "Confirm button configuration.",
         config: [
             {
                 configName: "text",
                 defaultValue: "Confirm",
-                type:"string",
+                type: "string",
                 explanation: "Confirm button text.",
             },
             {
                 configName: "bgColor",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Confirm button background color.",
             },
             {
                 configName: "borderColor",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Confirm button border color.",
             },
             {
                 configName: "textColor",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Confirm button text color.",
             },
             {
                 configName: "iconEnd",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Confirm button icon at the end.",
             },
             {
                 configName: "iconStart",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Confirm button icon at the start.",
             },
             {
                 configName: "closeOnClick",
                 defaultValue: false,
-                type:"boolean",
+                type: "boolean",
                 explanation: "Whether SilverBox closes on click or not."
             },
             {
                 configName: "showButton",
                 defaultValue: true,
-                type:"boolean",
+                type: "boolean",
                 explanation: "Show/Hide confirm button."
             },
         ]
@@ -124,55 +126,55 @@ const documentation = [
     {
         configName: "cancelButton",
         defaultValue: "emptyDefaultValue",
-        type:"object",
+        type: "object",
         explanation: "Cancel button configuration.",
         config: [
             {
                 configName: "text",
                 defaultValue: "Cancel",
-                type:"string",
+                type: "string",
                 explanation: "Cancel button text.",
             },
             {
                 configName: "bgColor",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Cancel button background color.",
             },
             {
                 configName: "borderColor",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Cancel button border color.",
             },
             {
                 configName: "textColor",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Cancel button text color.",
             },
             {
                 configName: "iconEnd",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Cancel button icon at the end.",
             },
             {
                 configName: "iconStart",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Cancel button icon at the start.",
             },
             {
                 configName: "closeOnClick",
                 defaultValue: false,
-                type:"boolean",
+                type: "boolean",
                 explanation: "Whether SilverBox closes on click or not."
             },
             {
                 configName: "showButton",
                 defaultValue: true,
-                type:"boolean",
+                type: "boolean",
                 explanation: "Show/Hide cancel button"
             },
         ]
@@ -180,55 +182,55 @@ const documentation = [
     {
         configName: "denyButton",
         defaultValue: "emptyDefaultValue",
-        type:"object",
+        type: "object",
         explanation: "Deny button configuration.",
         config: [
             {
                 configName: "text",
                 defaultValue: "Deny",
-                type:"string",
+                type: "string",
                 explanation: "Deny button text.",
             },
             {
                 configName: "bgColor",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Deny button background color.",
             },
             {
                 configName: "borderColor",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Deny button border color.",
             },
             {
                 configName: "textColor",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Deny button text color.",
             },
             {
                 configName: "iconEnd",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Deny button icon at the end.",
             },
             {
                 configName: "iconStart",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Deny button icon at the start.",
             },
             {
                 configName: "closeOnClick",
                 defaultValue: true,
-                type:"boolean",
+                type: "boolean",
                 explanation: "Whether SilverBox closes on click or not."
             },
             {
                 configName: "showButton",
                 defaultValue: false,
-                type:"boolean",
+                type: "boolean",
                 explanation: "Show/Hide deny button."
             },
         ]
@@ -236,79 +238,79 @@ const documentation = [
     {
         configName: "input",
         defaultValue: "emptyDefaultValue",
-        type:"object | array",
+        type: "object | array",
         explanation: `Input configuration. Can be an "object" or an "array" of "objects".`,
         config: [
             {
                 configName: "label",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Input label.",
             },
             {
                 configName: "type",
                 defaultValue: "text",
-                type:"string",
+                type: "string",
                 explanation: `Input type. It can be "text", "number", "textarea", ...`,
             },
             {
                 configName: "placeHolder",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Input place holder.",
             },
             {
                 configName: "hint",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Input hint at the bottom.",
             },
             {
                 configName: "readOnly",
                 defaultValue: false,
-                type:"boolean",
+                type: "boolean",
                 explanation: "Input readonly attribute.",
             },
             {
                 configName: "width",
                 defaultValue: "100%",
-                type:"string",
+                type: "string",
                 explanation: "Input width.",
             },
             {
                 configName: "height",
                 defaultValue: "100%",
-                type:"string",
+                type: "string",
                 explanation: "Input height.",
             },
             {
                 configName: "maxLength",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Input maxlength attribute."
             },
             {
                 configName: "textAlign",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Input text align."
             },
             {
                 configName: "fontSize",
                 defaultValue: "",
-                type:"string",
+                type: "string",
                 explanation: "Input font size."
             },
             {
                 configName: "multiplyBy",
                 defaultValue: "",
-                type:"number",
+                type: "number",
                 explanation: "Multiplies the input."
             },
             {
                 configName: "numberOnly",
                 defaultValue: false,
-                type:"boolean",
+                type: "boolean",
                 explanation: "Accept only numbers from user."
             },
             // {
@@ -321,4 +323,4 @@ const documentation = [
     }
 ];
 
-export default documentation
+export default sortDocumentation(documentation)
