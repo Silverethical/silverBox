af9cee52a48e3e8d3665efb317f7831ac4e5c17f
dofxo <kargat504@gmail.com>
2023-04-25 23:51:05 +0330

    refactor(silverBox): change removeLoadings conditions

--- public/src/js/helpers/silverBox/removeLoadings.js
+++ public/src/js/helpers/silverBox/removeLoadings.js
@@ -5,6 +5,10 @@ function silverBoxRemoveLoadings(animationIndex) {
     // converts the given value to lowerCase
     animationIndex.toLowerCase()
 
+    // covert the indexes 
+    if (animationIndex === 'first') animationIndex = 1
+    if (animationIndex === 'last') animationIndex = silverBoxButtonWrapper.length
+
     // removes all modal's button's loading
     if (animationIndex === 'all') {
         for (let i = 0; i < silverBoxButtonWrapper.length; i++) {
@@ -15,20 +19,6 @@ function silverBoxRemoveLoadings(animationIndex) {
         }
 
     }
-    // removes the first modal's button's loading
-    else if (animationIndex === 'first') {
-        silverBoxButtonWrapper[0].childNodes.forEach(button => {
-            button.classList.remove('silverBox-loading-button')
-        })
-    }
-
-    // removes the last modal's button's loading
-    else if (animationIndex === 'last') {
-        silverBoxButtonWrapper[silverBoxButtonWrapper.length - 1].childNodes.forEach(button => {
-            button.classList.remove('silverBox-loading-button')
-        })
-    }
-
     // removes the nth modal's button's loading
     else if (Number(animationIndex) > 0) {
         silverBoxButtonWrapper[Number(animationIndex) - 1].childNodes.forEach(button => {
