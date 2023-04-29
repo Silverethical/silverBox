a6e7d8bdc4be29155e059f98a3612021b737ab46
Majid Kargar <majidkargar01@gmail.com>
2023-04-29 14:21:06 +0430

    refactor(index): change table styles

--- public/src/js/components/documentationTableComponent.js
+++ public/src/js/components/documentationTableComponent.js
@@ -11,8 +11,9 @@ function silverBoxDocumentationTableComponent() {
 
     // document argument
     let silverBoxTableRowConfig = (selector, idValue, configValue) => silverBoxTableRow({
-        argument: selector.configName,
-        explanation: selector.explanation,
+        name: selector.configName,
+        type: selector.type,
+        description: selector.explanation,
         defaultValue: selector.defaultValue,
         id: idValue,
         config: configValue,
@@ -29,9 +30,6 @@ function silverBoxDocumentationTableComponent() {
             // creates document configs wrapper
             let documentConfigWrapper = document.createElement('div')
             documentConfigWrapper.classList.add('silverBox-document-has-config')
-            // create indicator
-            let documentConfigWrapperIndicator = document.createElement('span')
-            documentConfigWrapperIndicator.classList.add("silverBox-documentConfig-indicator")
 
             documentItem.config.forEach(config => {
                 let configTableRow = silverBoxTableRowConfig(config, "hasConfig", documentItem)
@@ -39,7 +37,6 @@ function silverBoxDocumentationTableComponent() {
                 documentConfigWrapper.append(configTableRow)
             });
             // appending the whole parent div after the related config explanation
-            tableWrapper.append(documentConfigWrapperIndicator)
             tableWrapper.append(documentConfigWrapper)
 
         }
@@ -55,25 +52,31 @@ function silverBoxTableHeader() {
     const tableHeader = document.createElement("div")
     tableHeader.classList.add("silverBox-tableRow", "silverBox-tableRowHeader")
 
-    // argument header
-    const argumentHeader = document.createElement("div")
-    argumentHeader.classList.add("silverBox-tableColumn", "tableHeaderColumn")
-    argumentHeader.textContent = "Argument"
+    // name header
+    const nameHeader = document.createElement("div")
+    nameHeader.classList.add("silverBox-tableColumn", "tableHeaderColumn")
+    nameHeader.textContent = "Name"
 
-    // default value header
-    const defaultValueHeader = document.createElement("div")
-    defaultValueHeader.classList.add("silverBox-tableColumn", "tableHeaderColumn")
-    defaultValueHeader.textContent = "Default Value"
+    // type header
+    const typeHeader = document.createElement('div')
+    typeHeader.classList.add("silverBox-tableColumn", "tableHeaderColumn")
+    typeHeader.textContent = "Type"
 
-    // default value header
-    const explanationHeader = document.createElement("div")
-    explanationHeader.classList.add("silverBox-tableColumn", "tableHeaderColumn")
-    explanationHeader.textContent = "Explanation"
+    // default header
+    const defaultHeader = document.createElement("div")
+    defaultHeader.classList.add("silverBox-tableColumn", "tableHeaderColumn")
+    defaultHeader.textContent = "Default"
+
+    // description header
+    const descriptionHeader = document.createElement("div")
+    descriptionHeader.classList.add("silverBox-tableColumn", "tableHeaderColumn")
+    descriptionHeader.textContent = "Description"
 
     // append all items
-    tableHeader.append(argumentHeader)
-    tableHeader.append(defaultValueHeader)
-    tableHeader.append(explanationHeader)
+    tableHeader.append(nameHeader)
+    tableHeader.append(typeHeader)
+    tableHeader.append(defaultHeader)
+    tableHeader.append(descriptionHeader)
 
     return tableHeader
 }
@@ -87,107 +90,116 @@ function silverBoxTableHeader() {
  * @param {string} id - determines the status of id (how the id is added)
  * @returns {element} - table row element
  */
-function silverBoxTableRow({ argument, explanation, defaultValue, id, config }) {
+function silverBoxTableRow({ name, type, description, defaultValue, id, config }) {
     // selectors
     const tableRowEl = document.createElement("div")
     tableRowEl.classList.add("silverBox-tableRow")
 
-    // argument column ----
+    // name column ----
 
-    const docArgumentColumn = document.createElement("a")
-    docArgumentColumn.classList.add("silverBox-tableColumn", "document-argument")
-    docArgumentColumn.textContent = argument
+    const docNameColumn = document.createElement("a")
+    docNameColumn.classList.add("silverBox-tableColumn", "document-argument")
+    docNameColumn.textContent = name
 
-    // copies the argument textContent after being Clicked
-    docArgumentColumn.onclick = () => { copyArgument(docArgumentColumn) }
+    // type column
+    const docTypeColumn = document.createElement("div")
+    docTypeColumn.classList.add("silverBox-tableColumn", "silverBox-document-type")
+    docTypeColumn.textContent = type
 
-    // explanation column ----
+    // copies the name textContent after being Clicked
+    docNameColumn.onclick = () => { copyArgument(docNameColumn) }
 
-    const docExplanationColumn = document.createElement("div")
-    docExplanationColumn.classList.add("silverBox-tableColumn", "silverBox-document-explanation")
-    const explanationSpan = document.createElement("span")
-    explanationSpan.classList.add("silverBox-explanation-span")
-    explanationSpan.innerHTML = replaceQuotedTextWithSpan(explanation)
-    docExplanationColumn.append(explanationSpan)
+    // description column ----
+
+    const docDescriptionColumn = document.createElement("div")
+    docDescriptionColumn.classList.add("silverBox-tableColumn", "silverBox-document-description")
+    const descriptionSpan = document.createElement("span")
+    docDescriptionColumn.classList.add("silverBox-explanation-span")
+    descriptionSpan.innerHTML = replaceQuotedTextWithSpan(description)
+    docDescriptionColumn.append(descriptionSpan)
 
     // default value column ----
 
     const docDefaultValueColumn = document.createElement("div")
-    docDefaultValueColumn.classList.add("silverBox-tableColumn")
+    docDefaultValueColumn.classList.add("silverBox-tableColumn", "silverBox-document-default")
 
     if (defaultValue != "emptyDefaultValue") {
+        docDefaultValueColumn.classList.add("silverBox-document-string")
+        docDefaultValueColumn.textContent = defaultValue
+
+        if (defaultValue === "") {
+            docDefaultValueColumn.classList.add("silverBox-document-string")
+            docDefaultValueColumn.textContent = '" "'
+        } 
         // if type of the default value is string this code will be executed
-        if (typeof defaultValue === "string") {
+        // if (typeof defaultValue === "string") {
             // if the string is empty this code will be executed
-            if (defaultValue === "") {
-                docDefaultValueColumn.classList.add("silverBox-document-string")
-                docDefaultValueColumn.textContent = '" "'
-            } else {
-                docDefaultValueColumn.textContent = `"${defaultValue}"`
-                docDefaultValueColumn.classList.add("silverBox-document-string")
-            }
-        }
+     
+            // else {
+            //     docDefaultValueColumn.textContent = `"${defaultValue}"`
+            //     docDefaultValueColumn.classList.add("silverBox-document-string")
+            // }
+        // }
         // if type of the default value is boolean this code will be executed
-        else if (typeof defaultValue === "boolean") {
-            docDefaultValueColumn.classList.add("silverBox-document-boolean")
-            docDefaultValueColumn.textContent = `${defaultValue}`
-        }
+        // else if (typeof defaultValue === "boolean") {
+        //     docDefaultValueColumn.classList.add("silverBox-document-boolean")
+        //     docDefaultValueColumn.textContent = `${defaultValue}`
+        // }
         // if type of the default value is object this code will be executed
-        else if (typeof defaultValue === "undefined") {
-            docDefaultValueColumn.classList.add("silverBox-document-undefined")
-            docDefaultValueColumn.textContent = `${defaultValue}`
-        }
+        // else if (typeof defaultValue === "undefined") {
+        //     docDefaultValueColumn.classList.add("silverBox-document-undefined")
+        //     docDefaultValueColumn.textContent = `${defaultValue}`
+        // }
     }
 
     // if id == "noConfig" this code will be executed
     if (id == "noConfig") {
-        docArgumentColumn.id = `${argument}`
-        docArgumentColumn.setAttribute("href", "#" + argument)
+        docDescriptionColumn.id = `${name}`
+        docDescriptionColumn.setAttribute("href", "#" + name)
 
         // if there is config keyword in the value of config which is documentation in our case
         // this code will be executed
         if ("config" in config) {
             // creates the show more button
-            let documentShowMore = document.createElement('span')
-            documentShowMore.innerHTML = "More"
-            documentShowMore.classList.add("silverBox-document-show-more")
-            docExplanationColumn.append(documentShowMore)
+            // create indicator
+            let documentConfigWrapperIndicator = document.createElement('div')
+            documentConfigWrapperIndicator.classList.add("silverBox-documentConfig-indicator")
+            docNameColumn.append(documentConfigWrapperIndicator)
         }
     }
     // if id == "hasConfig" this code will be executed
     else if (id == "hasConfig") {
         // gets the id of the configName that contains the config
-        docArgumentColumn.id = `${config.configName}${argument}`
-        docArgumentColumn.setAttribute("href", `#${config.configName}${argument}`)
+        docDescriptionColumn.id = `${config.configName}${name}`
+        docDescriptionColumn.setAttribute("href", `#${config.configName}${name}`)
 
     }
     // appends the items to table row
 
-    // argument
-    tableRowEl.append(docArgumentColumn)
+    // name
+    tableRowEl.append(docNameColumn)
+    // type
+    tableRowEl.append(docTypeColumn)
     // default value
     tableRowEl.append(docDefaultValueColumn)
-    // explanation
-    tableRowEl.append(docExplanationColumn)
+    // description
+    tableRowEl.append(docDescriptionColumn)
 
     return tableRowEl
 }
 function toggleConfig() {
     // select all configs
-    const showMoreBtn = document.querySelectorAll('.silverBox-document-show-more')
     const documentThatHasConfig = document.querySelectorAll('.silverBox-document-has-config')
     const silverBoxDocumentConfigIndicator = document.querySelectorAll('.silverBox-documentConfig-indicator')
 
     // for loop
-    for (let i = 0; i < showMoreBtn.length; i++) {
+    for (let i = 0; i < documentThatHasConfig.length; i++) {
         // click event for each showMoreBtn
-        showMoreBtn[i].addEventListener("click", () => {
+        silverBoxDocumentConfigIndicator[i].addEventListener("click", () => {
             // if each document that has config contains "hide" class this code will be executed
             if (documentThatHasConfig[i].classList.contains("silverBox-hide")) {
                 // remove "hide" class from document that has config
                 documentThatHasConfig[i].classList.remove("silverBox-hide")
-                // remove "hide" class from show more button
-                showMoreBtn[i].classList.remove("silverBox-hide")
                 // remove "hide" class from Document Config Indicator
                 silverBoxDocumentConfigIndicator[i].classList.remove("silverBox-hide")
             }
@@ -195,8 +207,6 @@ function toggleConfig() {
             else {
                 // add "hide" class to document that has config 
                 documentThatHasConfig[i].classList.add("silverBox-hide")
-                // add "hide" class to show more button 
-                showMoreBtn[i].classList.add("silverBox-hide")
                 // add "hide" class to Document Config Indicator
                 silverBoxDocumentConfigIndicator[i].classList.add("silverBox-hide")
             }
