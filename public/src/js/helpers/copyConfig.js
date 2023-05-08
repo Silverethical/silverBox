722ef8035ee59d551c8ae2fc4043cddb4dcedc15
Majid Kargar <majidkargar01@gmail.com>
2023-05-08 16:02:16 +0430

    refactor(helpers/copyConfig): change copyConfig conditions

--- public/src/js/helpers/copyConfig.js
+++ public/src/js/helpers/copyConfig.js
@@ -6,13 +6,13 @@
  * @param {number} timeOut - the amount of numbers that the text changes back to normal
  * after click
  */
-function copyConfig(copy, iconElement, timeOut) {
+function copyConfig({ copy, iconElement, timeOut }) {
     navigator.clipboard.writeText(copy);
     // changes the text to copied!
-    iconElement.classList.add('silverBox-copied')
+    if (iconElement) iconElement.classList.add('silverBox-copied')
     // changes it back to copy after 1s
     setTimeout(() => {
-        iconElement.classList.remove('silverBox-copied')
+        if (iconElement) iconElement.classList.remove('silverBox-copied')
     }, timeOut)
 }
 export default copyConfig
