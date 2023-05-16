9a89d3dcf0bbca8ad8e5873f179be65974bd6283
Saber Heydari <silverethical@gmail.com>
2023-05-16 09:54:03 +0330

    feat(general): update node modules, add babel and css minification

---
+++ rollup.config.js
@@ -0,0 +1,62 @@
+import { babel } from "@rollup/plugin-babel";
+
+const config = [
+	{
+		input: "./public/src/js/libraries/highlightJS/highlight.min.js",
+		output: {
+			file: "./public/dist/js/libraries/highlightJS/highlight.min.js",
+			format: "es",
+			sourcemap: "inline",
+		},
+		plugins: [
+			babel({
+				babelHelpers: "bundled",
+				presets: ["@babel/preset-env"],
+			}),
+		],
+	},
+	{
+		input: "./public/src/js/index.js",
+		output: {
+			file: "./public/dist/js/index.js",
+			format: "es",
+			sourcemap: "inline",
+		},
+		plugins: [
+			babel({
+				babelHelpers: "bundled",
+				presets: ["@babel/preset-env"],
+			}),
+		],
+	},
+	{
+		input: "./public/src/js/silverBox.js",
+		output: {
+			file: "./public/dist/js/silverBox.js",
+			format: "es",
+			sourcemap: "inline",
+		},
+		plugins: [
+			babel({
+				babelHelpers: "bundled",
+				presets: ["@babel/preset-env"],
+			}),
+		],
+	},
+	{
+		input: "./public/src/js/documentationPage.js",
+		output: {
+			file: "./public/dist/js/documentationPage.js",
+			format: "es",
+			sourcemap: "inline",
+		},
+		plugins: [
+			babel({
+				babelHelpers: "bundled",
+				presets: ["@babel/preset-env"],
+			}),
+		],
+	},
+];
+
+export default config;
