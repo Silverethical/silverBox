9f13f93780cfbb57190bfa22dca81837b6b6f1c2
Majid Kargar <majidkargar01@gmail.com>
2023-05-31 21:09:07 +0430

    docs(components/loadingAnimation): add comments

--- public/src/js/components/silverBox/loadingAnimation.js
+++ public/src/js/components/silverBox/loadingAnimation.js
@@ -3,8 +3,13 @@
  * @returns {Element} - loading animation element
  */
 function silverBoxLoadingAnimation() {
+	// create loading element
 	const loadingEl = document.createElement("span");
+
+	// add className to loading element
 	loadingEl.classList.add("silverBox-button-loading-animation");
+
+	// return loading element
 	return loadingEl;
 }
 
