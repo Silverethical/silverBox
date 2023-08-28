63ad368a22407700ca38a26c834811e6a373b44c
Majid Kargar <majidkargar01@gmail.com>
2023-08-28 14:29:56 +0430

    refactor(animation): Refactor and enhance applyAnimation function.
    Description:
    
    The convertToMiliSeconds function converts provided value to ms if it's a number, else returns the value.
    It alos handles the conversion of number values to strings with the 'ms' suffix for milliseconds. Additionally, a regular expression has been introduced to match any alphabetic character at the end of a string, ensuring consistent suffix addition.
    
    The normalizedConfig object has been refactored to include the animation configuration merging between defaultValues and config. The duration and delay properties are now normalized using the convertToMiliSeconds function to ensure consistent formatting with the 'ms' suffix. If the conversion returns falsy values (e.g., undefined), the default values are used as fallbacks.

--- public/src/js/helpers/silverBox/applyAnimation.js
+++ public/src/js/helpers/silverBox/applyAnimation.js
@@ -7,15 +7,42 @@ const applyAnimation = (config) => {
 	// default values for animation properties
 	const defaultValues = {
 		name: "popUp",
-		duration: ".3s",
+		duration: "300ms",
 		timingFunction: "linear",
-		delay: "0s",
+		delay: "0ms",
 		iterationCount: "1",
 		direction: "normal",
 		fillMode: "none",
 	};
 
-	// destructuring animation config key
+	// Convert number values to strings with 's' suffix for seconds
+	const convertToMilSeconds = (value) => {
+		const regex = /[a-zA-Z]$/; // Regular expression to match any alphabetic character at the end of a string
+
+		// If the value is a number
+		if (typeof value === "number") {
+			// Return the value with "ms" suffix
+			return `${value}ms`;
+		}
+
+		// If the value is a string and does not match the regex
+		if (typeof value === "string" && !regex.test(value)) {
+			// Return the value with "ms" suffix added
+			return `${value}ms`;
+		}
+
+		return value; // Return the value as is (no modification needed)
+	};
+
+	// Normalize duration and delay values
+	const normalizedConfig = {
+		...defaultValues,
+		...config,
+		duration: convertToMilSeconds(config.duration) || defaultValues.duration,
+		delay: convertToMilSeconds(config.delay) || defaultValues.delay,
+	};
+
+	// Destructure animation config keys
 	const {
 		name,
 		duration,
@@ -24,7 +51,7 @@ const applyAnimation = (config) => {
 		iterationCount,
 		direction,
 		fillMode,
-	} = { ...defaultValues, ...config };
+	} = normalizedConfig;
 
 	return `${name} ${duration} ${timingFunction} ${delay} ${iterationCount} ${direction} ${fillMode}`;
 };
