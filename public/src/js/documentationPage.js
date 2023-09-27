1d8f468a15de061cec6dec59c0cedde6c433d429
Majid Kargar <majidkargar01@gmail.com>
2023-09-27 20:08:39 +0330

    Feat(documentation): Add new version

--- public/src/js/documentationPage.js
+++ public/src/js/documentationPage.js
@@ -36,7 +36,7 @@ window.addEventListener("scroll", () => {
 });
 
 // all versions of documentation
-const allVersions = ["1.0.0", "1.1.0", "1.2.0"];
+const allVersions = ["1.0.0", "1.1.0", "1.2.0", "1.3.0"];
 
 // version number from query string url
 let versionNumber = getParameterByName("v");
